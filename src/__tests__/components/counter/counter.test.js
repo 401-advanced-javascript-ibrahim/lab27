a8dc1e8fafe6db4b5a145a8f173116560350cbba
ibraim-Ajarmeh <ajarmeh1997@gmail.com>
2020-02-24 22:56:28 +0200

    fixed the bug

--- src/__tests__/components/counter/counter.test.js
+++ src/__tests__/components/counter/counter.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
-import Counter from '../../components/counter/counter.js';
+import Counter from '../../../components/counter/counter.js';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -10,12 +10,19 @@ describe('<Counter />', () => {
     it('can render counter', () => {
         let app = mount(<Counter />);
         let span = app.find('span');
-        expect(span).toBeDefiend();
+        expect(span.exists()).toBeTruthy();
         expect(app.state('count')).toBe(0);
     });
 
     it('can render to the DOM', () => {
         const rendered = renderer.create(<Counter />).toJSON();
         expect(rendered).toMatchSnapshot();
-    })
-})
+    });
+
+    it('can change the counter on click', () => {
+        let app = mount(<Counter />);
+        let span = app.find('.down');
+        span.simulate('click');
+        expect(app.state('count')).toBe(-1)
+    });
+});
