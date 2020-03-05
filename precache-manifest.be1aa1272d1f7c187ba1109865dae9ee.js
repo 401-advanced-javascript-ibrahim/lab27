7024c36d561b2fd2e082caa329bec0725c24f3db
ibrahim-hikari <runner@fv-az33.xpyq2erabo5erm1ixr3ud215dg.cx.internal.cloudapp.net>
2020-03-05 11:34:50 +0000

    Updates

---
+++ precache-manifest.be1aa1272d1f7c187ba1109865dae9ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b908e299c7867b199c61e519d6d6dc0a",
+    "url": "/401-advanced-javascript-ibrahim/lab27/index.html"
+  },
+  {
+    "revision": "749eec1199943858d6b1",
+    "url": "/401-advanced-javascript-ibrahim/lab27/static/js/2.344b3e7f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/401-advanced-javascript-ibrahim/lab27/static/js/2.344b3e7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2e88e1bdb747167eafb",
+    "url": "/401-advanced-javascript-ibrahim/lab27/static/js/main.3abec5d9.chunk.js"
+  },
+  {
+    "revision": "fea20d7940ba5af9d082",
+    "url": "/401-advanced-javascript-ibrahim/lab27/static/js/runtime-main.6daadb50.js"
+  }
+]);
